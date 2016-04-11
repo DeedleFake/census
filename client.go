53ca90f012d9fef89e3ca8f7389ffd3cd74e8857
DeedleFake <deedlefake@users.noreply.github.com>
2016-04-11 15:43:58 -0400

    Add godoc comments.

--- client.go
+++ client.go
@@ -5,17 +5,28 @@ import (
 	"net/http"
 )
 
+// A Client is the main interface for the Census API.
 type Client struct {
+	// ServiceID is the ID to use when making API requests. If it is
+	// blank, DefaultServiceID is used.
 	ServiceID string
-	Game      string
 
+	// Game is the namespace to use. Examples are `ps2`, `ps2ps4us`,
+	// `eq2`, etc.
+	Game string
+
+	// HTTPClient is the client to use for making API requests. If it is
+	// nil, http.DefaultClient is used.
 	HTTPClient *http.Client
 }
 
+// buildURL runs BuildURL with the appropriate arguments pulled from
+// fields of cl.
 func (cl Client) buildURL(verb, col string, opts ...URLOption) string {
 	return BuildURL(cl.ServiceID, verb, cl.Game, col, opts...).String()
 }
 
+// c gets the http.Client, handling the nil case.
 func (cl Client) c() *http.Client {
 	if cl.HTTPClient == nil {
 		return http.DefaultClient
@@ -24,6 +35,14 @@ func (cl Client) c() *http.Client {
 	return cl.HTTPClient
 }
 
+// Fetch returns the raw JSON that the API returns for a request. If
+// the API itself has an error, such as
+//
+//     {"error":"No data found."}
+//
+// then the returned error will be an *Error.
+//
+// For more information about the arguments, see BuildURL.
 func (cl Client) Fetch(verb, col string, opts ...URLOption) ([]byte, error) {
 	url := cl.buildURL(verb, col, opts...)
 	rsp, err := cl.c().Get(url)
@@ -50,6 +69,11 @@ func (cl Client) Fetch(verb, col string, opts ...URLOption) ([]byte, error) {
 	return raw, nil
 }
 
+// Get performs a request to the API using a `get` verb. It decodes
+// the returned JSON into dst.
+//
+// Like with Fetch, if the API yields an error, the returned error
+// will be of type *Error.
 func (cl Client) Get(dst interface{}, col string, opts ...URLOption) error {
 	raw, err := cl.Fetch("get", col, opts...)
 	if err != nil {
@@ -59,6 +83,12 @@ func (cl Client) Get(dst interface{}, col string, opts ...URLOption) error {
 	return json.Unmarshal(raw, dst)
 }
 
+// Count performs a request to the API using a `count` verb. It
+// returns the returned count and an error. If an error is
+// encountered, the value of the returned int is undefined.
+//
+// Like with Fetch, if the API yields an error, the returned error
+// will be of type *Error.
 func (cl Client) Count(col string, opts ...URLOption) (int, error) {
 	raw, err := cl.Fetch("count", col, opts...)
 	if err != nil {
@@ -72,6 +102,7 @@ func (cl Client) Count(col string, opts ...URLOption) (int, error) {
 	return data.Count, err
 }
 
+// An Error represents an error that is returned by the API.
 type Error struct {
 	Err string `json:"error"`
 }
--- url.go
+++ url.go
@@ -9,12 +9,22 @@ import (
 )
 
 const (
-	urlBase          = "census.daybreakgames.com"
+	urlBase = "census.daybreakgames.com"
+
+	// DefaultServiceID is the ID which is used when none is specified.
 	DefaultServiceID = "example"
 )
 
+// A URLOption is an option that is passed to the API.
 type URLOption func(url.Values)
 
+// BuildURL builds a request URL from the various components.
+//
+//     id:   The service ID.
+//     verb: The type of request. Usually `get` or `count`.
+//     game: The namespace. For example, `ps2`.
+//     col:  The collection type. For example, `character`.
+//     opts: A list of options. See the example for more information.
 func BuildURL(id, verb, game, col string, opts ...URLOption) *url.URL {
 	q := make(url.Values, len(opts))
 	for _, opt := range opts {
@@ -33,6 +43,13 @@ func BuildURL(id, verb, game, col string, opts ...URLOption) *url.URL {
 	}
 }
 
+// SearchOption is a search request. For example,
+//
+//     SearchOption("name.first_lower", "deedlefaketr")
+//
+// produces
+//
+//     name.first_lower=deedlefaketr
 func SearchOption(name, val string) URLOption {
 	return func(q url.Values) {
 		q.Set(name, val)
@@ -46,14 +63,17 @@ func listOption(name string, vals ...string) URLOption {
 	}
 }
 
+// ShowOption is a list of fields to show. It cooresponds to `c:show`.
 func ShowOption(fields ...string) URLOption {
 	return listOption("c:show", fields...)
 }
 
+// HideOption is a list of fields to hide. It cooresponds to `c:hide`.
 func HideOption(fields ...string) URLOption {
 	return listOption("c:hide", fields...)
 }
 
+// A Sort is a mapping of field names to sort directions.
 type Sort struct {
 	Field string
 	Dir   SortDir
@@ -73,14 +93,22 @@ func (s Sort) String() string {
 	return buf.String()
 }
 
+// A SortDir is a direction to sort a field.
 type SortDir int
 
 const (
+	// Desc is descending order.
 	Desc SortDir = iota - 1
+
+	// DefaultDir is an unspecified order.
 	DefaultDir
+
+	// Asc is ascending order.
 	Asc
 )
 
+// SortOption is a list of fields to sort by. It cooresponds to
+// `c:sort`.
 func SortOption(sorts ...Sort) URLOption {
 	var buf bytes.Buffer
 	for _, sort := range sorts {
@@ -94,20 +122,27 @@ func SortOption(sorts ...Sort) URLOption {
 	}
 }
 
+// HasOption filters objects by the presence of fields. It cooresponds
+// to `c:has`.
 func HasOption(fields ...string) URLOption {
 	return listOption("c:has", fields...)
 }
 
+// ResolveOption is a list of resolves. It cooresponds to `c:resolve`.
 func ResolveOption(fields ...string) URLOption {
 	return listOption("c:resolve", fields...)
 }
 
+// IgnoreCaseOption makes searches case-insensetive. It cooresponds to
+// `c:case=false`.
 func IgnoreCaseOption() URLOption {
 	return func(q url.Values) {
 		q.Set("c:case", "false")
 	}
 }
 
+// LimitOption limits the number of returned results. It cooresponds
+// to `c:limit`.
 func LimitOption(limit int) URLOption {
 	str := strconv.FormatInt(int64(limit), 10)
 	return func(q url.Values) {
@@ -115,6 +150,8 @@ func LimitOption(limit int) URLOption {
 	}
 }
 
+// LimitPerDBOption is a different type of limit on the number of
+// returned result than LimitOption. It cooresponds to `c:limitPerDB`.
 func LimitPerDBOption(limit int) URLOption {
 	str := strconv.FormatInt(int64(limit), 10)
 	return func(q url.Values) {
@@ -122,6 +159,8 @@ func LimitPerDBOption(limit int) URLOption {
 	}
 }
 
+// StartOption specifies the index of the first result. It cooresponds
+// to `c:start`.
 func StartOption(start int) URLOption {
 	str := strconv.FormatInt(int64(start), 10)
 	return func(q url.Values) {
@@ -129,12 +168,16 @@ func StartOption(start int) URLOption {
 	}
 }
 
+// IncludeNullOption enables null field values. It cooresponds to
+// `c:includeNull=true`.
 func IncludeNullOption() URLOption {
 	return func(q url.Values) {
 		q.Set("c:includeNull", "true")
 	}
 }
 
+// LangOption specifies what language to return in multi-lingual
+// results. It cooresponds to `c:lang`.
 func LangOption(lang string) URLOption {
 	return func(q url.Values) {
 		q.Set("c:lang", lang)
@@ -142,26 +185,35 @@ func LangOption(lang string) URLOption {
 }
 
 // TODO: Implement JoinOption.
+
 // TODO: Implement TreeOption.
 
+// TimingOption adds a field in the top-level struct that gives the
+// time the API servers spent fetching data from the database. It cooresponds to `c:timing=true`.
 func TimingOption() URLOption {
 	return func(q url.Values) {
 		q.Set("c:timing", "true")
 	}
 }
 
+// ExactMatchFirstOption specifies that an exact match should always
+// be the first result. It cooresponds to `c:exactMatchFirst=true`.
 func ExactMatchFirstOption() URLOption {
 	return func(q url.Values) {
 		q.Set("c:exactMatchFirst", "true")
 	}
 }
 
+// DistinctOption specifies that all distinct values of a field should
+// be returned. It cooresponds to `c:distinct`.
 func DistinctOption(field string) URLOption {
 	return func(q url.Values) {
 		q.Set("c:distinct", field)
 	}
 }
 
+// NoRetryOption specifies that the API shouldn't automatically retry
+// on failures. It cooresponds to `c:retry=false`.
 func NoRetryOption() URLOption {
 	return func(q url.Values) {
 		q.Set("c:retry", "false")
